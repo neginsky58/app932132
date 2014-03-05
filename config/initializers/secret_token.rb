a8a8f23370d464d87828723f060d72d0bc3b0a59
root <neginsky58@gmail.com>
2014-03-05 10:57:06 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Binbuds::Application.config.secret_key_base = '31cd256b30a47d5f8b7f88f5a3fe86c8a5347185753b701eaf4f215ef02efe44b020beab46c35283a0906e08f8350489651302f0d96c0d24058b6fb18dd9494f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Binbuds::Application.config.session_store :cookie_store, key: '_binbuds_session'

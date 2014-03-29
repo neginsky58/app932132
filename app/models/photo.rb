1c5bb4586a5d8dfae981384bd90a4a1a8fbf4bc3
root <neginsky58@gmail.com>
2014-03-29 15:27:11 +0200

    s3 credentials

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -10,7 +10,7 @@ class Photo < ActiveRecord::Base
 
   has_attached_file :file, 
     :storage => :s3,
-    :s3_credentials => "#{Rails.root}/config/s3.yml",
+    :s3_credentials => { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'] },
     :styles => { :medium => "1024x768>", :thumb => "100x100>" }, 
     :default_url => "no_image.jpg",
     :bucket => 'BidBuds'

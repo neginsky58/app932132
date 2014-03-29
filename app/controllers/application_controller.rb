957a659ee006040bd0ee2ff016ae9d9ec5f61b80
root <neginsky58@gmail.com>
2014-03-29 18:51:07 +0200

    skip verify token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   
   # protect_from_forgery with: :exception
 
-  # skip_before_filter :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token
   
   layout :layout_by_resource
 
